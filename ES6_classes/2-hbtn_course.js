32ee409486f80dec7d0e6c4dea49ec1f3376f4cf
carlosalfredo4200 <alfredomontoyacorreo2@gmail.com>
2023-09-15 09:07:36 -0500

    2-hbtn_course.js

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,16 +1,13 @@
 class HolbertonCourse {
     constructor(name, length, students) {
-
-        if (typeof name !== 'string' || typeof length !== 'number' || !Array.isArray(students)) {
-            throw new Error('Invalid argument types');
+        if (typeof name !== 'string' || typeof length !== 'number' || length <= 0 || !Array.isArray(students) || students.length === 0) {
+            throw new Error('Invalid arguments. Name must be a string, length must be a positive number, and students must be a non-empty array');
         }
-
 
         this._name = name;
         this._length = length;
         this._students = students;
     }
-
 
     get name() {
         return this._name;
@@ -23,28 +20,42 @@ class HolbertonCourse {
         this._name = newName;
     }
 
-
     get length() {
         return this._length;
     }
 
     set length(newLength) {
-        if (typeof newLength !== 'number') {
-            throw new Error('Length must be a number');
+        if (typeof newLength !== 'number' || newLength <= 0) {
+            throw new Error('Length must be a positive number');
         }
         this._length = newLength;
     }
-
 
     get students() {
         return this._students;
     }
 
     set students(newStudents) {
-        if (!Array.isArray(newStudents)) {
-            throw new Error('Students must be an array');
+        if (!Array.isArray(newStudents) || newStudents.length === 0) {
+            throw new Error('Students must be a non-empty array');
         }
         this._students = newStudents;
     }
 }
 
+const c1 = new HolbertonCourse("ES6", 1, ["Bob", "Jane"]);
+console.log(c1.name);
+c1.name = "Python 101";
+console.log(c1);
+
+try {
+    c1.name = 12;
+} catch(err) {
+    console.log(err);
+}
+
+try {
+    const c2 = new HolbertonCourse("ES6", "1", ["Bob", "Jane"]);
+} catch(err) {
+    console.log(err);
+}
