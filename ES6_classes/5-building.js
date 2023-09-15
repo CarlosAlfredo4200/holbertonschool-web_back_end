3bd527dde1e3eb8fa44de4fc69b2374c5ead0ede
carlosalfredo4200 <alfredomontoyacorreo2@gmail.com>
2023-09-15 09:57:18 -0500

    5-building.js

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,8 +1,5 @@
 class Building {
   constructor(sqft) {
-    if (this.constructor === Building) {
-      throw new Error('Abstract classes cannot be instantiated directly');
-    }
     this._sqft = sqft;
   }
 
@@ -10,11 +7,10 @@ class Building {
     return this._sqft;
   }
 
-  /* eslint-disable class-methods-use-this */
   evacuationWarningMessage() {
+    
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }
-  /* eslint-enable class-methods-use-this */
 }
 
 export default Building;
