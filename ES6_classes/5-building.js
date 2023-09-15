3810eeae65d208a75248c2e76cdf6026198a7011
carlosalfredo4200 <alfredomontoyacorreo2@gmail.com>
2023-09-15 10:11:27 -0500

    5-building.js

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,23 +1,18 @@
-class Building {
+export default class Building {
   constructor(sqft) {
+    if (
+      this.constructor !== Building
+        && typeof this.evacuationWarningMessage !== 'function'
+    ) {
+      throw Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
+    }
+
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
   }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-  }
 }
-
-export default Building;
-
-class TestBuilding extends Building {}
-
-test('building forces override', () => {
-  expect(() => {
-    new TestBuilding(200).evacuationWarningMessage();
-  }).toThrow('Class extending Building must override evacuationWarningMessage');
-});
