ae63201779c8cdc8705bd85cc97cca9965bd0fdf
carlosalfredo4200 <alfredomontoyacorreo2@gmail.com>
2023-09-15 09:15:10 -0500

    2-hbtn_course.js

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,61 +1,54 @@
-class HolbertonCourse {
+// 2-hbtn_course.js
+export default class HolbertonCourse {
     constructor(name, length, students) {
-        if (typeof name !== 'string' || typeof length !== 'number' || length <= 0 || !Array.isArray(students) || students.length === 0) {
-            throw new Error('Invalid arguments. Name must be a string, length must be a positive number, and students must be a non-empty array');
-        }
-
-        this._name = name;
-        this._length = length;
-        this._students = students;
+      if (
+        typeof name !== 'string' ||
+        typeof length !== 'number' ||
+        length <= 0 ||
+        !Array.isArray(students) ||
+        students.length === 0
+      ) {
+        throw new Error(
+          'Invalid arguments. Name must be a string, length must be a positive number, and students must be a non-empty array'
+        );
+      }
+  
+      this._name = name;
+      this._length = length;
+      this._students = students;
     }
-
+  
     get name() {
-        return this._name;
+      return this._name;
     }
-
+  
     set name(newName) {
-        if (typeof newName !== 'string') {
-            throw new Error('Name must be a string');
-        }
-        this._name = newName;
+      if (typeof newName !== 'string') {
+        throw new Error('Name must be a string');
+      }
+      this._name = newName;
     }
-
+  
     get length() {
-        return this._length;
+      return this._length;
     }
-
+  
     set length(newLength) {
-        if (typeof newLength !== 'number' || newLength <= 0) {
-            throw new Error('Length must be a positive number');
-        }
-        this._length = newLength;
+      if (typeof newLength !== 'number' || newLength <= 0) {
+        throw new Error('Length must be a positive number');
+      }
+      this._length = newLength;
     }
-
+  
     get students() {
-        return this._students;
+      return this._students;
     }
-
+  
     set students(newStudents) {
-        if (!Array.isArray(newStudents) || newStudents.length === 0) {
-            throw new Error('Students must be a non-empty array');
-        }
-        this._students = newStudents;
+      if (!Array.isArray(newStudents) || newStudents.length === 0) {
+        throw new Error('Students must be a non-empty array');
+      }
+      this._students = newStudents;
     }
-}
-
-const c1 = new HolbertonCourse("ES6", 1, ["Bob", "Jane"]);
-console.log(c1.name);
-c1.name = "Python 101";
-console.log(c1);
-
-try {
-    c1.name = 12;
-} catch(err) {
-    console.log(err);
-}
-
-try {
-    const c2 = new HolbertonCourse("ES6", "1", ["Bob", "Jane"]);
-} catch(err) {
-    console.log(err);
-}
+  }
+  
