d1db4550bb3009d2198e3e246a582530fc0b2acf
carlosalfredo4200 <alfredomontoyacorreo2@gmail.com>
2023-10-05 15:48:03 -0500

    getStudentsByLocation

--- prueba.js
+++ prueba.js
@@ -1,10 +1,19 @@
 function getListStudents() {
     return [
-        { firstName: 'Guillaume', id: 1, location: 'San Francisco' },
-        { firstName: 'James', id: 2, location: 'Columbia' },
-        { firstName: 'Serena', id: 5, location: 'San Francisco' },
+      { firstName: 'Guillaume', id: 1, location: 'San Francisco' },
+      { firstName: 'James', id: 2, location: 'Columbia' },
+      { firstName: 'Serena', id: 5, location: 'San Francisco' },
     ];
+  }
+  
+
+
+function getStudentsByLocation(arr, search) {
+    return arr.filter((el) => el.location === search )
 }
 
 
 
+const students = getListStudents();
+
+console.log(getStudentsByLocation(students, 'San Francisco' ));
